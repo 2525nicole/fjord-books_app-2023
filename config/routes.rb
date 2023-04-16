8c6369a52e02f906e82e635eec12bdff826210e4
nicole2525 <nicole2525nomail@gmail.com>
2023-04-16 17:39:22 +0900

    ログアウト機能追加、devise-i18nをインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :books
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
