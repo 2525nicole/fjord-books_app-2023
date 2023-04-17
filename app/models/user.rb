cf4690670f83bf3b822a864f9ecd8049566d53f3
nicole2525 <nicole2525nomail@gmail.com>
2023-04-17 21:53:01 +0900

    自動翻訳されていない部分の国際化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
   validates :postal_code, format: { with: /\A\d{3}-?\d{4}\z/ }, allow_blank: true
 end
