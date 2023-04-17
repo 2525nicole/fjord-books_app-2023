5f325a5d637a674fc5736034217cf7ee657519ba
nicole2525 <nicole2525nomail@gmail.com>
2023-04-17 09:09:07 +0900

    ユーザー一覧/詳細ページを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all.page(params[:page]).per(10).order(updated_at: :desc, id: :asc)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
+  resources :users, only: [:index, :show]
   resources :books
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
