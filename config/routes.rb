7f31419c7ee6b602112123c61a0daf0782986f04
nicole2525 <nicole2525nomail@gmail.com>
2023-05-05 21:56:23 +0900

    インデントや記述に関する修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   resources :comments
   resources :reports do
-    resources :comments, only: [:create, :destroy, :update, :edit], module: :reports
+    resources :comments, only: [:edit, :create, :update, :destroy], module: :reports
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, only: [:create, :destroy, :update, :edit], module: :books
+    resources :comments, only: [:edit, :create, :update, :destroy], module: :books
   end
   resources :users, only: %i(index show)
 end
