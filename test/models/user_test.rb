91af3ef1b4152faf8366ee7707714d85f1c73494
nicole2525 <nicole2525nomail@gmail.com>
2023-06-01 15:03:05 +0900

    Userのモデルテストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:alice)
+  end
+
+  test "name_or_email_should_return_email" do
+    assert_equal 'alice@example.com', @user.name_or_email
+  end
+
+  test "name_or_email_should_return_name" do
+    @user.name = 'alice'
+    assert_equal 'alice', @user.name_or_email
+  end
 end
