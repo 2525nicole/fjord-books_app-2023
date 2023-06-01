731b2ba7bc48902bbeeb12c8effab3c474e527cc
nicole2525 <nicole2525nomail@gmail.com>
2023-06-01 15:29:40 +0900

    Reportのモデルテストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,28 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @report = reports(:one)
+  end
+
+  test "editable?" do
+    target_user = users(:alice)
+    assert @report.editable?(target_user) 
+
+    target_user = users(:bob)
+    assert_not @report.editable?(target_user) 
+  end
+
+  test "created_on" do
+    assert_instance_of( Date, @report.created_on )
+
+    assert_not_instance_of( Date, @report.created_at )
+  end
+
+  test "save_mentions" do
+    assert_changes -> { @report.mentioning_report_ids }, from: [2], to: [3,4] do
+      @report.content = 'http://localhost:3000/reports/1,http://localhost:3000/reports/3,http://localhost:3000/reports/4'
+      @report.send(:save_mentions)
+    end
+  end
 end
